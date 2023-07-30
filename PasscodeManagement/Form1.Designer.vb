b00a60222193104870b35da21f22300b6025eb56
Wasim Khan <wasimkhan0816@gmail.com>
2023-07-30 20:05:00 +0530

    Added all the source code and all related file to it

---
+++ PasscodeManagement/Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(704, 115)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(167, 31)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Stored Password"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(704, 256)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(167, 30)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Show Password"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(29, 118)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(336, 22)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(382, 119)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(295, 22)
+        Me.TextBox2.TabIndex = 3
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(382, 256)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(295, 103)
+        Me.RichTextBox1.TabIndex = 5
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(29, 99)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 17)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Account Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(379, 99)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 17)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Password"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(29, 257)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(336, 22)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(29, 237)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(100, 17)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Account Name"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(324, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(206, 17)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Passcode Management System"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 72)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(167, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "To Stored your password"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(29, 211)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(157, 17)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "To know your password"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(29, 416)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(169, 17)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "To update your password"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(379, 449)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(127, 17)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Updated Password"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(29, 449)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(100, 17)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Account Name"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(382, 469)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(295, 22)
+        Me.TextBox4.TabIndex = 16
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(29, 468)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(336, 22)
+        Me.TextBox5.TabIndex = 15
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(704, 465)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(167, 31)
+        Me.Button3.TabIndex = 14
+        Me.Button3.Text = "Set updated Password"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(887, 536)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Password Manager"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button3 As Button
+End Class
---
+++ PasscodeManagement/Form1.vb
@@ -0,0 +1,100 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+
+    Dim FileRead As StreamReader
+    Dim FileWrite As StreamWriter
+    Dim accountname As String
+    Dim passcode As String
+    Dim getfiletxt As String
+    Dim fname As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        accountname = TextBox1.Text
+        passcode = TextBox2.Text
+        Try
+            If TextBox1.Text = "" Or TextBox2.Text = "" Then
+                MessageBox.Show("Please enter the account name and Password")
+                Exit Sub
+            End If
+            accountname = TextBox1.Text.Trim.ToLower
+            fname = "E:\PROJECTS\VS\PasscodeManagement\passcode\" + accountname + ".txt"
+            If System.IO.File.Exists(fname) Then
+                MessageBox.Show(accountname + " account name already exist please use another account name")
+                Exit Sub
+            Else
+                FileWrite = My.Computer.FileSystem.OpenTextFileWriter("E:\PROJECTS\VS\PasscodeManagement\passcode\" + accountname + ".txt", True)
+
+                FileWrite.WriteLine("Account Name: " + accountname + " Password: " + passcode)
+                FileWrite.Close()
+                MessageBox.Show("Password stored successfully against " + accountname + " account")
+                Button1.Enabled = False
+                TextBox1.ReadOnly = True
+                TextBox2.ReadOnly = True
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        accountname = TextBox3.Text
+        Try
+            If TextBox3.Text = "" Then
+                MessageBox.Show("Please enter the account name")
+                Exit Sub
+            End If
+            accountname = TextBox3.Text.Trim.ToLower
+            fname = "E:\PROJECTS\VS\PasscodeManagement\passcode\" + accountname + ".txt"
+            If Not System.IO.File.Exists(fname) Then
+                MessageBox.Show(accountname + " account is not found, please enter the valid account name")
+                Exit Sub
+            Else
+                FileRead = My.Computer.FileSystem.OpenTextFileReader("E:\PROJECTS\VS\PasscodeManagement\passcode\" + accountname + ".txt")
+                getfiletxt = FileRead.ReadToEnd()
+                RichTextBox1.Text = getfiletxt
+                FileRead.Close()
+                Button2.Enabled = False
+                RichTextBox1.ReadOnly = True
+                TextBox3.ReadOnly = True
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim updfilename As String
+        accountname = TextBox5.Text
+        passcode = TextBox4.Text
+        Try
+            If TextBox4.Text = "" Or TextBox5.Text = "" Then
+                MessageBox.Show("Please enter the existing account name and set updated Password")
+                Exit Sub
+            End If
+            accountname = TextBox5.Text.Trim.ToLower
+            updfilename = "E:\PROJECTS\VS\PasscodeManagement\passcode\" + accountname + ".txt"
+
+            If Not System.IO.File.Exists(updfilename) Then
+                MessageBox.Show(accountname + " account is not found, please enter the valid account name")
+            Else
+                System.IO.File.Delete(updfilename)
+                FileWrite = My.Computer.FileSystem.OpenTextFileWriter(updfilename, True)
+                FileWrite.WriteLine("Account Name: " + accountname + " Password: " + passcode)
+                FileWrite.Close()
+                MessageBox.Show("Password updated successfully against " + accountname + " account")
+                Button3.Enabled = False
+                TextBox4.ReadOnly = True
+                TextBox5.ReadOnly = True
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+End Class
